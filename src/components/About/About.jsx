c3bda51493bbfe1ba42c7303c70f4e9117ff58ed
mani-prakash-007 <manip8072@gmail.com>
2024-08-26 23:34:34 +0530

    Completed About and Skills components

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,39 @@
 import React from "react";
+import Photo2 from "../../assets/Photo3.jpg";
+import Education from "../../assets/Education.png";
+import Experience from "../../assets/Experience.png";
 
 export const About = () => {
-  return <div className="border border-black h-180 p-10">About</div>;
+  return (
+    <div className="border border-black h-180 p-10 flex justify-between">
+      <div className="border border-black w-96 h-144 items-center flex">
+        <img src={Photo2} alt="Photo - 2" className="rounded-xl border border-black" />
+      </div>
+      <div className="border border-black w-3/4 h-144 p-10 flex flex-col justify-between">
+        <div className="border border-black w-auto h-60 px-20 flex justify-between items-center">
+          <div className="border border-black h-36 w-108 rounded-3xl flex justify-center">
+            <div className="flex flex-col justify-center items-center">
+              <img src={Experience} className="w-10 h-10" alt="Svg" />
+              <h1 className="font-bold text-xl">Experience</h1>
+              <p className=" tracking-wider">Fresher</p>
+            </div>
+          </div>
+          <div className="border border-black h-36 w-108 rounded-3xl flex justify-center">
+            <div className="flex flex-col justify-center items-center">
+              <img src={Education} className="w-10 h-10" alt="" />{" "}
+              <h1 className="font-bold text-xl">Education</h1>
+              <p className=" tracking-wider">B.E Computer Science and Engineering</p>
+            </div>
+          </div>
+        </div>
+        <div className="border border-black w-auto h-60 py-5 px-10">
+          <p className="text-lg">I'm Mani Prakash, a third-year Computer Science student specializing
+          in Web Development, Artificial Intelligence and Machine Learning. I'm
+          passionate about technology and driven to push boundaries. I
+          continuously hone my skills to tackle real-world challenges and
+          leverage technology to drive meaningful change and innovation.</p>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,7 +1,93 @@
-import React from 'react'
+import React from "react";
+import { RiVerifiedBadgeFill } from "react-icons/ri";
 
 export const Skills = () => {
+  const programmingLanguagesKnown = [
+    "Javascript",
+    "Java",
+    "Python",
+    "C (Basics)",
+  ];
+  const frameworksKnown = ["React JS", "Node JS", "Express JS", "Tailwind CSS"];
+  const toolsKnown = [
+    "Version Control",
+    "Redux Toolkit",
+    "VS Code",
+    "Netbeans",
+  ];
+  const softSkills = [
+    "Communication Skills",
+    "Leadership",
+    "Presentation",
+    "Team Work",
+    "Adaptablity",
+  ];
   return (
-    <div className="border border-black h-180 p-10">Skills</div>
-  )
-}
+    <div className="border border-black h-180 p-10 flex justify-between">
+      <div className="border border-black w-96 h-144 rounded-3xl p-10 flex">
+        <div className="border border-black w-full h-full">
+          <h1 className="text-center font-bold text-2xl my-5">
+            Programming Languages
+          </h1>
+          <ul className="border border-black h-96 mx-5 flex flex-col justify-evenly pl-5">
+            {programmingLanguagesKnown.map((skill) => {
+              return (
+                <div className="flex">
+                  <RiVerifiedBadgeFill className="w-7 h-7 mx-5" />
+                  <li className="font-bold text-lg">{skill}</li>
+                </div>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="border border-black w-96 h-144 rounded-3xl p-10 flex">
+        <div className="border border-black w-full h-full">
+          <h1 className="text-center font-bold text-2xl my-5">
+            Frameworks & Library
+          </h1>
+          <ul className="border border-black h-96 my-5 mx-5 flex flex-col justify-evenly pl-5">
+            {frameworksKnown.map((skill) => {
+              return (
+                <div className="flex">
+                  <RiVerifiedBadgeFill className="w-7 h-7 mx-5" />
+                  <li className="font-bold text-lg">{skill}</li>
+                </div>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="border border-black w-96 h-144 rounded-3xl p-10 flex">
+        <div className="border border-black w-full h-full">
+          <h1 className="text-center font-bold text-2xl my-5">Tools</h1>
+          <ul className="border border-black h-96 my-5 mx-5 flex flex-col justify-evenly pl-5">
+            {toolsKnown.map((skill) => {
+              return (
+                <div className="flex">
+                  <RiVerifiedBadgeFill className="w-7 h-7 mx-5" />
+                  <li className="font-bold text-lg">{skill}</li>
+                </div>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="border border-black w-96 h-144 rounded-3xl p-10 flex">
+        <div className="border border-black w-full h-full">
+          <h1 className="text-center font-bold text-2xl my-5">Soft Skills</h1>
+          <ul className="border border-black h-96 my-5 mx-5 flex flex-col justify-evenly pl-5">
+            {softSkills.map((skill) => {
+              return (
+                <div className="flex">
+                  <RiVerifiedBadgeFill className="w-7 h-7 mx-5" />
+                  <li className="font-bold text-lg">{skill}</li>
+                </div>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
